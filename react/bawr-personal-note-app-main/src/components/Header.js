f79485431ecb66e11cdd6829e2ff74135c6fab73
tafifa <tafifa09@gmail.com>
2023-06-12 03:47:28 +0700

    delete some files

--- react/bawr-personal-note-app-main/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export default function Header() {
-  return (
-    <header className="bg-primary py-4 text-center text-white shadow-md">
-      <h1 className="text-lg font-bold lg:text-xl">Catatanku</h1>
-    </header>
-  );
-}
--- react/bawr-personal-note-app-main/src/components/Toast.js
+++
@@ -1,6 +0,0 @@
-import clsx from 'clsx';
-import * as React from 'react';
-
-export default function Toast({ message, show }) {
-  return <div className={clsx('toast success', { show })}>{message}</div>;
-}
